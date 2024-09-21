5a226940aa340bfe4d778fc66bd3405117834d75
aliOrifjonovich <orifjonovichalisher@gmail.com>
2024-09-21 18:27:53 +0500

    feat: changed ui in header

--- src/components/LanguageSelection/LanguageSelection.tsx
+++ src/components/LanguageSelection/LanguageSelection.tsx
@@ -1,19 +1,34 @@
 import { LanguageIcon } from '@/Icons';
+import { EnLangIcon } from '@/Icons/EnLangIcon';
+import { RuLangIcon } from '@/Icons/RuLangIcon';
+import { UzLangIcon } from '@/Icons/UzLangIcon';
 import Cookies from 'js-cookie';
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 
 const LanguageSelection: FC = () => {
   const langs = [
     {
       label: 'ru',
+      icon: <RuLangIcon />
     },
     {
       label: 'uz',
+      icon: <UzLangIcon />
     },
     {
       label: 'en',
+      icon: <EnLangIcon />
     },
   ];
+
+  // eslint-disable-next-line no-undef
+  const [selectedLang, setSelectedLang] = useState<{ label: string; icon: JSX.Element } | null>(null);
+
+  useEffect(() => {
+    const localeCookie = Cookies.get('locale') || 'en';
+    const lang = langs.find((item) => item.label === localeCookie);
+    setSelectedLang(lang || langs[2]); 
+  }, []); 
 
   const handleChangeLang = (lang: string) => {
     Cookies.set('locale', lang);
@@ -22,24 +37,23 @@ const LanguageSelection: FC = () => {
 
   return (
     <div className="group relative">
-      <div className="cursor-pointer">
-        <span className="">en</span>
-        <span className="">
+      <div className="cursor-pointer flex items-center gap-1 border border-[#C3D4E9] p-1 rounded-md">
+        <span className="">{selectedLang?.icon}</span> 
+        <span className="uppercase text-lg font-medium">{selectedLang?.label}</span>
+        <span>
           <LanguageIcon />
         </span>
       </div>
 
-      <div className="absolute z-30 hidden flex-col border border-red-500 bg-white p-5 group-hover:flex">
+      <div className="absolute z-30 hidden flex-col bg-white p-2 group-hover:flex">
         {langs.map((lang) => (
           <div
             key={lang.label}
-            className=""
+            className="flex items-center gap-2 cursor-pointer hover:bg-gray-100 p-2"
             onClick={() => handleChangeLang(lang.label)}
           >
-            <a className="">
-              {lang.label}
-              <span className=""></span>
-            </a>
+            <span>{lang.icon}</span> {/* Display the language icon */}
+            <span className='uppercase text-md font-medium'>{lang.label}</span>
           </div>
         ))}
       </div>
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -2,11 +2,11 @@
 
 import LanguageSelection from '@/components/LanguageSelection/LanguageSelection';
 import { Search } from '@/components/Search';
+import { LikeIcon } from '@/Icons';
+import { NotificationIcon } from '@/Icons/NotificationIcon';
 import { Container } from '@mui/material';
-import { useTranslations } from 'next-intl';
 
 const Header = () => {
-  const t = useTranslations('HomePage');
   return (
     <Container>
       <header className="flex items-center justify-between">
@@ -14,9 +14,16 @@ const Header = () => {
           <h1>iRent</h1>
           <Search />
         </div>
-        <div>
+        <div className='flex items-center gap-5'>
           <LanguageSelection />
-          <h1>{t('title')}</h1>
+          <span className='border border-[#C3D4E9] p-2 rounded-full cursor-pointer '><LikeIcon/></span>
+          <div className='border border-[#C3D4E9] p-2 rounded-full cursor-pointer relative'>
+            <span className='bg-[#FF4423] w-3 h-3 rounded-full absolute top-[-2px] right-[-2px]'></span>
+            <NotificationIcon/>
+          </div>
+        </div>
+        <div className=''>
+        <img src="" alt="" />
         </div>
       </header>
     </Container>
